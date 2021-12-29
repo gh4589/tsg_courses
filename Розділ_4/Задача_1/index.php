919826f5ea44c7840106ed6ffa5faf20f9cea63b
GH4589 <vasya.rebrik@gmail.com>
2021-12-29 21:12:45 +0200

    commit init

--- Розділ_4/Задача_1/index.php
+++ Розділ_4/Задача_1/index.php
@@ -1,40 +1,12 @@
 <!DOCTYPE html>
-<html lang="ru">
+<html lang="en">
 <head>
-    
-    <meta http-equiv="Content-Type" content="text/html; charset=windows-1251" />
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Задача_1</title>
+    <title>Document</title>
 </head>
 <body>
-
-<div class="container">
-<h1>Введіть дані </h1>
-<div class="container__inner">
-<div class="container__form-select">
-
-<form action="" method="get">
-
-
-<div class="container__form-input">
-    Введіть обєм двигуна :<input type="text" name="listofnames"  method="get"><BR>
-    <button style="margin-top:10px;" type="submit">Відправити</button>
-
-
-</form>
-</div>
-
-
-<?php  include('ini.php') ?>
-
-
-</div>
-
-
-
-</div>
-
-
-</style>
+    
 </body>
 </html>
